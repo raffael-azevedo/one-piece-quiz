0ff9b1b98e9567682ffd06c4e145a9e3208f2293
Raffael Azevedo <raffael.azevedo@gmail.com>
2024-04-18 14:41:51 -0300

    small changes; added new css rules

--- js/game.js
+++ js/game.js
@@ -48,6 +48,7 @@ startGame = () => {
 
 getNewQuestion = () => {
   if (questionCounter >= MAX_QUESTIONS || availableQuestions.length === 0) {
+    localStorage.setItem("mostRecentScore", score);
     return window.location.assign("/end.html");
   }
   questionCounter++;
