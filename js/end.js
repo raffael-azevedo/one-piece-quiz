7070d6bb0b4d19d644fa39b4e773d0cbbb422d8d
Raffael Azevedo <raffael.azevedo@gmail.com>
2024-04-18 15:05:01 -0300

    now saving high scores to the local storage

--- js/end.js
+++ js/end.js
@@ -2,6 +2,8 @@ const username = document.getElementById("usr");
 const saveScoreButton = document.getElementById("saveScoreBtn");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const finalScore = document.getElementById("finalScore");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -11,4 +13,17 @@ username.addEventListener("keyup", () => {
 
 saveHighScore = (e) => {
   e.preventDefault();
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+  // sorting to get always the max and removing the lowest
+  highScores.sort((a, b) => {
+    return b.score - a.score;
+  });
+  highScores.splice(5);
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  window.location.assign("index.html");
 };
